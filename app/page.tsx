878fc4dc1a6b3b6e3c8cfcebb1bdb863f045ed3f
AbyvargheseMandapathel <abyvarghesemandapathel@gmail.com>
2025-04-14 22:35:31 +0530

    build

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,32 @@
 import Layout from "@/components/layout/Layout"
-import Section1 from '@/components/sections/home1/Section1'
-import Section10 from '@/components/sections/home1/Section10'
-import Section11 from '@/components/sections/home1/Section11'
-import Section2 from '@/components/sections/home1/Section2'
-import Section3 from '@/components/sections/home1/Section3'
-import Section4 from '@/components/sections/home1/Section4'
-import Section5 from '@/components/sections/home1/Section5'
-import Section6 from '@/components/sections/home1/Section6'
-import Section7 from '@/components/sections/home1/Section7'
-import Section8 from '@/components/sections/home1/Section8'
-import Section9 from '@/components/sections/home1/Section9'
-export default function Home() {
+import Section1 from '@/components/sections/home12/Section1'
+import Section10 from '@/components/sections/home12/Section10'
+import Section2 from '@/components/sections/home12/Section2'
+import Section3 from '@/components/sections/home12/Section3'
+import Section4 from '@/components/sections/home12/Section4'
+import Section5 from '@/components/sections/home12/Section5'
+import Section6 from '@/components/sections/home12/Section6'
+import Client from '@/components/sections/home12/Client'
+import Section7 from '@/components/sections/home12/Section7'
+import Section8 from '@/components/sections/home12/Section8'
+import Section9 from '@/components/sections/home12/Section9'
+export default function Home12() {
 
 	return (
 		<>
 
-			<Layout headerStyle={1} footerStyle={1}>
+			<Layout headerStyle={12} footerStyle={17}>
 				<Section1 />
 				<Section2 />
-				<Section3 />
+				<Client />
+				{/* <Section3 />
 				<Section4 />
 				<Section5 />
 				<Section6 />
 				<Section7 />
 				<Section8 />
-				<Section9 />
-				<Section10 />
-				<Section11 />
+				<Section9 /> */}
+				{/* <Section10 /> */}
 			</Layout>
 		</>
 	)
