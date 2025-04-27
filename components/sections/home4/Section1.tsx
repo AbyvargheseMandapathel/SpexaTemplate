427f1296a5d62b5dcb17f770f0b71241f0c5d9f5
AbyvargheseMandapathel <abyvarghesemandapathel@gmail.com>
2025-04-27 21:01:06 +0530

    responsive hero

--- components/sections/home4/Section1.tsx
+++ components/sections/home4/Section1.tsx
@@ -57,14 +57,16 @@ export default function Section1() {
 					backgroundSize: 'cover',
 					backgroundPosition: 'center',
 					transition: 'background-image 1s ease-in-out',
-					height: '100vh', // Fixed height to eliminate empty space
-					minHeight: '600px' // Minimum height for smaller screens
+					height: 'auto', // Changed from fixed height to auto
+					minHeight: '100vh', // Ensure it takes at least full viewport height
+					paddingTop: '80px',
+					paddingBottom: '80px'
 				}}
 			>
 				<div className="overlay"></div>
 				<div className="container h-100 d-flex align-items-center" data-aos="fade-up">
 					<div className="row align-items-center">
-						<div className="col-lg-7 mb-4">
+						<div className="col-lg-7 col-md-12 mb-4">
 							<div className="banner-2">
 								<div className="sub-heading-ag-sm color-white text-uppercase mb-20" data-aos="fade-up">
 									<svg width={13} height={13} viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -102,19 +104,19 @@ export default function Section1() {
 								<p className="paragraph-rubik-r color-white desc-banner mb-40" data-aos="fade-up">
 									{currentSlide.description}
 								</p>
-								<div className="d-flex align-items-center" data-aos="fade-up">
-									<Link href="#" className="btn btn-primary-square-2-md">
+								<div className="d-flex flex-wrap align-items-center" data-aos="fade-up">
+									<Link href="#" className="btn btn-primary-square-2-md mb-3 mb-sm-0 me-sm-3">
 										Schedule a consultation
 										<img src="/assets/imgs/template/icons/plus-sm.svg" alt="Vatech" />
 									</Link>
 									
-									<Link href={currentSlide.buttonLink} className="btn btn-outline-light ms-3">
+									<Link href={currentSlide.buttonLink} className="btn btn-outline-light mb-3 mb-sm-0">
 										{currentSlide.buttonText}
 									</Link>
 								</div>
 							</div>
 						</div>
-						<div className="col-lg-5 mb-4 text-center" data-aos="fade-right">
+						<div className="col-lg-5 col-md-12 mb-4 text-center" data-aos="fade-right">
 							{/* Updated image styling */}
 							<motion.div 
 								className="box-banner-image"
@@ -164,7 +166,7 @@ export default function Section1() {
 				}
 				
 				.heading-container {
-					min-height: 150px;
+					min-height: 120px;
 					display: flex;
 					align-items: center;
 				}
@@ -188,6 +190,8 @@ export default function Section1() {
 					display: block;
 					transition: transform 0.5s ease;
 					border-radius: 10px;
+					max-height: 500px;
+					object-fit: contain;
 				}
 				
 				.image-highlight {
@@ -234,9 +238,65 @@ export default function Section1() {
 					transform: translateY(-3px);
 				}
 				
+				/* Improved mobile responsiveness */
 				@media (max-width: 991px) {
 					.heading-container {
-						min-height: 100px;
+						min-height: 80px;
+					}
+					
+					.display-xl {
+						font-size: calc(2rem + 1.5vw) !important;
+						line-height: 1.2;
+					}
+					
+					.desc-banner {
+						font-size: 1rem;
+					}
+				}
+				
+				@media (max-width: 767px) {
+					.heading-container {
+						min-height: 60px;
+					}
+					
+					.display-xl {
+						font-size: calc(1.8rem + 1vw) !important;
+					}
+					
+					.box-banner-image {
+						max-width: 90%;
+						padding: 10px;
+					}
+					
+					.featured-image {
+						max-height: 350px;
+					}
+					
+					.btn {
+						width: 100%;
+						margin-right: 0 !important;
+						margin-left: 0 !important;
+						text-align: center;
+						justify-content: center;
+					}
+				}
+				
+				@media (max-width: 575px) {
+					.display-xl {
+						font-size: calc(1.5rem + 1vw) !important;
+					}
+					
+					.sub-heading-ag-sm {
+						font-size: 0.8rem;
+					}
+					
+					.featured-image {
+						max-height: 250px;
+					}
+					
+					.box-banner-4 {
+						padding-top: 60px;
+						padding-bottom: 60px;
 					}
 				}
 			`}</style>
